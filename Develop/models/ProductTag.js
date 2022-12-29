f5dfbe51db206f8d6724e0565d42a29436a00f37
j-faust <85812553+j-faust@users.noreply.github.com>
2022-12-29 14:44:25 -0500

    Added initial code to models folder

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -6,7 +6,20 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    // defined columns for database
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Product',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
